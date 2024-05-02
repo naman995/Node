1e5430f2fdbda09d3a9569163167f12f2540912a
naman995 <rajputnaman823@gmail.com>
2024-05-03 00:59:08 +0530

    Refactor index.js to add logRequest middleware and update personRoutes import

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+// Middleware Function
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleDateString()}] Request made to : ${req.originUrl}`
+  );
+};
+
+app.get("/", logRequest, (req, res) => {
+  res.send("Welcome to our hotel");
+});
+
+// Import Router Files
 const personRoutes = require("./routes/personRoutes");
 const menuRoutes = require("./routes/menuRoutes");
 
+// Use the routes
 app.use("/person", personRoutes);
 app.use("/menuItems", menuRoutes);
 
